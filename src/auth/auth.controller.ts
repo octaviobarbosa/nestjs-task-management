0a6aa4fb442e0ad39f2db79a1dc15294c6ee50e8
Octavio Barbosa <octavio.barbosa@tecnospeed.com.br>
2021-10-28 23:45:16 -0300

    feat: signing in

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,4 +9,9 @@ export class AuthController {
   signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.authService.signUp(authCredentialsDto);
   }
+
+  @Post('/signin')
+  signIN(@Body() authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    return this.authService.signIn(authCredentialsDto);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UsersRepository } from './users.repository';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -11,5 +12,17 @@ export class AuthService {
 
   async signUp(authCredentiaslDto: AuthCredentialsDto): Promise<void> {
     return this.usersRepository.createUser(authCredentiaslDto);
+  }
+
+  async signIn(authCredentiaslDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentiaslDto;
+
+    const user = await this.usersRepository.findOne({ username });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'success';
+    } else {
+      throw new UnauthorizedException('Please check your login credentials');
+    }
   }
 }
