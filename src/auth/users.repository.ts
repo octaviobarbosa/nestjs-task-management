6208d3560f13f6ad5d62a3325b28d363b545d71e
Octavio Barbosa <octavio.barbosa@tecnospeed.com.br>
2021-10-28 23:08:53 -0300

    Feature: Signing Up

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,7 +1,17 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
-  //
+  async createUser(authCredentiaslDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentiaslDto;
+
+    const user = this.create({
+      username,
+      password,
+    });
+
+    await this.save(user);
+  }
 }
